2f482a392abf6b2b4494ed7e574020a802f3849e
carnasci <cen.master24@gmail.com>
2025-03-03 19:58:15 -0600

    Day 38 - application successfully creates new row to google sheets based on user input, trying to figure out why it only adds one exercise per input even if given 2 and have authenticated the google sheets request

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
-from credentials import app_id, app_key
+from datetime import datetime
+from credentials import app_id, app_key, sheety_endpoint, sheety_auth
 
 APP_ID = app_id
 APP_KEY = app_key
 API_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+SHEETY_ENDPOINT = sheety_endpoint
+SHEETY_AUTH = sheety_auth
+
 query = input("What exercises did you do today?: ")
 
 headers = {
@@ -17,5 +21,28 @@ json_params = {
     "age" : 36,
 }
 
+today = datetime.now().strftime("%m/%d/%Y")
+today_time = datetime.now().strftime("%H:%M:%S")
+print(today_time)
+print(today)
 response = requests.post(url=API_ENDPOINT, json=json_params, headers=headers)
 print(response.text)
+data = response.json()
+
+for exercise in data["exercises"]:
+    sheety_inputs = {
+        "workout" : {
+            "date": today,
+            "time": today_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+sheety_header = {
+    "Authorization" : SHEETY_AUTH,
+}
+
+sheety_response = requests.post(url=SHEETY_ENDPOINT, json=sheety_inputs, headers=sheety_header)
+print(sheety_response.text)
